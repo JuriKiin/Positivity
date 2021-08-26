2689342b46522c788850cae831255c61072ee1f8
jurikiin <kiin.juri@gmail.com>
2021-08-25 23:24:13 -0400

    2021 Revamp

--- main.js
+++ main.js
@@ -1,65 +1,34 @@
-//Create array for messages.
-var messages = ["Keep it up!", "You got this!", "You're working so hard!", "I appreciate you!","Proud of you!",
+const messages = [
+   "Keep it up!", "You got this!", "You're working so hard!", "I appreciate you!","Proud of you!",
    "Hang in there!", "I like how your mind works.","You're doing great!","Make sure you drink some water!","Go get it!",
    "You look nice today!","Happy thoughts!", "Have you done something nice today?", "Who have you helped today?", 
-   "Sometimes you are the most important person", "Stay focused!", "Make sure to do something fun today!", "Try something new today.",
+   "Sometimes you are the most important person.", "Stay focused!", "Make sure to do something fun today!", "Try something new today.",
    "You can accomplish anything.", "You have a great smile!", "I love you.", "Don't give up!", "You're almost there!",
-   "Turn that frown upside down!", "Be brave!"
-];  
-var background = document.querySelector('body');
-var isVisible = false;
+   "Turn that frown upside down!", "Be brave!", "You're so talented", "What a year you're having!", "Who told you to look that good?!",
+   "Look out! Successful person over here.", "Smile!!", "Give it all you got.", "Do something nice for someone today.", 
+   "Have you taken care of yourself today?"
+];
 
-window.onload = function() { //Call the function to change the message.
-   console.log("LOAD");
-   var messageNumber = GetRandomNumber();  //Get a random number between 0 and the length of the message array.
-   var messageLine = document.getElementById("message");   //Find the element with the correct id.
-   messageLine.innerText = messages[messageNumber];    //Set the text of that paragraph element.
-   background.style.backgroundColor = getRandomColor();
-   var image = document.getElementById("smile");
-   image.style.color = 'black';
-   ToggleAbout();
+window.onload = function() {
+   document.getElementById("message").textContent = messages[randomNumber(0, messages.length - 1)];
+   document.body.style.background = getRandomColor();
 };  
 
-window.addEventListener(onresize,setImageWidth);
-
-function setImageWidth() {
-   if(window.innerWidth < 700)
-   {
-       var smileImage = document.querySelector('#smile');
-       smileImage.style.width = window.innerWidth-200;
-   }
-}
-
-function openURL(url) {
-   window.open(url,'_self');
-}
-
-var close = document.getElementById('close');
-close.onclick = function() {
-    ToggleAbout();
-}
+function randomNumber(min, max) { 
+   return Math.floor(Math.random() * (max - min) + min);
+} 
 
 function getRandomColor() {
-   var red = Math.round(Math.random()*254+1);
-   var green = Math.round(Math.random()*254+1);
-   var blue=Math.round(Math.random()*254+1);
-   var color='rgba('+red+','+green+','+blue+',0.50)'; // 0.50 alpha
-
-   return color;
+   return 'rgba('+ randomNumber(0, 255)+','+ randomNumber(0, 255)+','+ randomNumber(0, 255)+', 0.6)';
 }
 
-function GetRandomNumber() { //Gets a random number.
-   return Math.floor(Math.random() * (messages.length));
-}
-
-function ToggleAbout() {
-   var aboutDiv = document.getElementById('About');
-   isVisible = !isVisible;
-   if(isVisible) {
-       aboutDiv.classList.remove('d-initial');
-       aboutDiv.classList.add('d-none');
+function toggleAbout(show) {
+   let aboutDiv = document.getElementById('About');
+   if (show) {
+      aboutDiv.classList.remove('d-none');
+      aboutDiv.classList.add('d-initial');
    } else {
-    aboutDiv.classList.remove('d-none');
-    aboutDiv.classList.add('d-initial');
+      aboutDiv.classList.remove('d-initial');
+      aboutDiv.classList.add('d-none');
    }
 }
